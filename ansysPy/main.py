10af0574ee35e8ccb441c9239793c2095ab1033b
thamilthedal <57757731+thamilthedal@users.noreply.github.com>
2023-08-01 10:52:08 +0530

    Update main.py

--- ansysPy/main.py
+++ ansysPy/main.py
@@ -1,28 +1,14 @@
+from meshData import mesh_data_extract
+from utils import extract_data, get_filenames
 import h5py
-import numpy as np
-from utils import print_group, extract_data, get_filenames
 
-# 3 D Data-extraction
+# Mesh Data extracted for 2D mesh
+f = h5py.File('src/test2.cas.h5', 'r')
+printgroup(f)
+face_Data, cell_Data = mesh_data_extract(f)
 
-#print("\n\n\nCase File Structure")
-f = h5py.File('src/test.cas.h5', 'r')
-#print_group(f)
-data = f['meshes']['1']['cells']['zoneTopology']['dimension']
-
-print(data[0])
-
-#Partition ID
-data = f['meshes']['1']['cells']['partition']['1']['partition-ids']
-ID = np.array(data)
-print("ID Obtained")
-
-print(ID[0])
-
-fileNames = get_filenames('src', 'dat.h5')
-for i in fileNames:
-    g = h5py.File(i, 'r')
-    #print_group(g)
-    [Cell_Variables, Cell_Data] = extract_data(g, 'cells')
-    [Face_Variables, Face_Data] = extract_data(g, 'faces')
-    print(i.split('/')[-1].split('.dat.h5')[0])
-    print(f"Max Temperature on Faces: {max(Face_Data[12])}")
+# Variable Data extraction for 3D or 2D data
+g = h5py.File('src/test.dat.h5', 'r')
+printgroup(g)
+[Cell_Variables, Cell_Data] = extract_data(g, 'cells')
+[Face_Variables, Face_Data] = extract_data(g, 'faces')
